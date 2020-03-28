40f0416e847c9b37eed7263f04b1d89398158a2e
pinq- <karli.kund@gmail.com>
2020-03-28 20:58:33 +0200

    Added new lines

--- js/Elo_tilastos.js
+++ js/Elo_tilastos.js
@@ -26,22 +26,32 @@ function get_json(){
 
 function parse_player_data(data){
   var stats = {};
+  start_points = 0;
   // console.log(data.length);
   $("#games_n").text(data.length);
   $.each(data, function(i,val){
     // console.log(val.player1);
+    if(start_points == 0){
+      start_points = (val.elo1 + val.elo2)/2;
+    }
+    date = new Date(val.input_date);
+    date.setMinutes(date.getMinutes() - 30);
+    date = date.getTime();
     if(typeof stats[val.player1] === 'undefined'){
-      stats[val.player1] = {tulokset: [], Elo_kehitys:[1600], Elo_nyt:1600, Voitot:0, Haviot:0, Tasa:0}
+      stats[val.player1] = {tulokset: [], Elo_kehitys:[start_points], pelin_aika: [date], Elo_nyt:start_points, Voitot:0, Haviot:0, Tasa:0}
     };
     if(typeof stats[val.player2] === 'undefined'){
-      stats[val.player2] = {tulokset: [], Elo_kehitys:[1600], Elo_nyt:1600, Voitot:0, Haviot:0, Tasa:0}
+      stats[val.player2] = {tulokset: [], Elo_kehitys:[start_points], pelin_aika: [date], Elo_nyt:start_points, Voitot:0, Haviot:0, Tasa:0}
     };
+    date = new Date(val.input_date).getTime();
     stats[val.player1].tulokset.push(val.score1);
-    stats[val.player1].Elo_kehitys.push(Number((val.elo1).toFixed(2)));
-    stats[val.player1].Elo_nyt = Number((val.elo1).toFixed(2));
+    stats[val.player1].Elo_kehitys.push(Number((val.elo1).toFixed(1)));
+    stats[val.player1].Elo_nyt = Number((val.elo1).toFixed(1));
     stats[val.player2].tulokset.push(val.score2);
-    stats[val.player2].Elo_kehitys.push(Number((val.elo2).toFixed(2)));
-    stats[val.player2].Elo_nyt = Number((val.elo2).toFixed(2));
+    stats[val.player2].Elo_kehitys.push(Number((val.elo2).toFixed(1)));
+    stats[val.player2].Elo_nyt = Number((val.elo2).toFixed(1));
+    stats[val.player1].pelin_aika.push(date);
+    stats[val.player2].pelin_aika.push(date);
     if(val.score1 > val.score2){
       stats[val.player1].Voitot ++;
       stats[val.player2].Haviot ++;
@@ -55,6 +65,10 @@ function parse_player_data(data){
       stats[val.player1].Tasa ++;
     };
   });
+  $.each(stats, function(i, val){
+    val.Elo_kehitys.push(val.Elo_kehitys[val.Elo_kehitys.length -1]);
+    val.pelin_aika.push(new Date().getTime());
+  });
   piirra_elo_kayra(stats);
   fill_table(stats);
   // console.log(stats);
@@ -64,7 +78,8 @@ function piirra_elo_kayra(data){
     objects = [];
     $.each(data, function(i,val){
       // console.log(i,val.Elo_kehitys);
-      objects.push({name:i, data:val.Elo_kehitys});
+      elo_points = val.pelin_aika.map((item,index) => {return [item,val.Elo_kehitys[index]]})
+      objects.push({name:i, data:elo_points});
     });
     // console.log(objects)
   Highcharts.chart('Elo_pisteet', {
@@ -80,10 +95,14 @@ function piirra_elo_kayra(data){
       },
 
       xAxis: {
-        tickInterval: 1,
+        // tickInterval: 1,
+        type: 'datetime',
         title: {
             text: "pelien määrä"
-        }
+        },
+        labels: {
+  format: '{value:%d.%m.%Y}',
+}
       },
       series: objects,
 
@@ -105,7 +124,7 @@ function fill_table(data){
     oka_sum = val.tulokset.reduce((a,b) => a + b, 0);
     objects.push({Name:i, Elo:val.Elo_nyt, Oka:Number((oka_sum/val.tulokset.length).toFixed(2)), win:val.Voitot, los: val.Haviot, even:val.Tasa});
   });
-  console.log(objects);
+  // console.log(objects);
   table.clear();
   table.rows.add(objects).draw();
 };
