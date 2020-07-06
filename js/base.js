1355e21c2a76412d7835e76361d81e9d72d5f294
pinq- <karli.kund@gmail.com>
2020-07-06 11:35:28 +0300

    matomo toinen testi

--- js/base.js
+++ js/base.js
@@ -1,16 +1,5 @@
 $( document ).ready(function() {
 
-  var _paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//pinq.kapsi.fi/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
   $('body').tooltip({selector: 'th'});
   $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
     $("body").toggleClass("sidebar-toggled");
